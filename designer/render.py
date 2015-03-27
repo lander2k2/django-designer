8154ff9df72cabe2d9a572e40bc0adac830106c8
Richard Lander <richard.lander@tribridge.com>
2015-03-27 11:50:43 -0400

    name change

--- designer/render.py
+++ designer/render.py
@@ -3,17 +3,19 @@ import os
 
 from django.template import Context, Template
 
-def render(template_name):
+def template(template_name):
 
     c = Context({})
 
     with open(template_name, 'r') as f:
         t = Template(f.read())
 
+    write_path = template_name.replace('.html', '_rendered.html')
+
     with open('rendered_template.html', 'w') as f:
         f.write(t.render(c))
 
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-
-    print 'file://{0}/rendered_template.html'.format(this_dir)
+    rendered_path = os.path.abspath(write_path)
+    
+    print 'file://{0}'.format(rendered_path)
 
